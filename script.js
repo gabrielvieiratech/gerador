8a1c714918b8eb9629b5aa2ff79807ecd15714ba
Gabriel <=>
2024-04-21 23:27:26 -0300

    Primeira versão

---
+++ script.js
@@ -0,0 +1,29 @@
+let sliderElement = document.querySelector('#slider');
+let buttonElement = document.querySelector('#button');
+let sizePassword = document.querySelector('#valor');
+let password = document.querySelector('#password');
+let containerPassword = document.querySelector('#container-password');
+
+
+let charset = 'abcdefghijklmnopqrstuvwxwzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@';
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+sliderElement.oninput = function() { // Corrigido
+  sizePassword.innerHTML = this.value;
+}
+
+function generatePassword() {
+  let pass = '';
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i ) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  novaSenha = pass; // Atribui a senha gerada à variável novaSenha
+  containerPassword.classList.remove('hide');
+  password.innerHTML = pass;
+}
+
+function copyPassword() {
+  alert('Senha copiada com sucesso!');
+  navigator.clipboard.writeText(novaSenha);
+}
